4c3eef1f278741852a7b803c976af7db05f92922
Farhan Niloy <farhansarkerniloy@gmail.com>
2023-01-20 15:35:02 +0600

    Just because it was anoying

--- script.js
+++ script.js
@@ -9,13 +9,16 @@
 //
 // console.log(document.querySelector('.highScore').textContent = '23');
 
+const number = Math.trunc(Math.random() * 20 + 1);
+document.querySelector('.number').textContent = number;
+
+
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number (document.querySelector('.guess').value);
   console.log(guess, typeof guess);
 
+
   if (!guess) {
     console.log(document.querySelector('.message').textContent = '🙈 No Guess!');
   }
-})
-
-console.log('helloworld')
+});
